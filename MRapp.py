722b723444e449f46bacc787aa6b9a4de6217abb
mrostar <mrostar2002@gmail.com>
2025-04-24 16:54:03 -0400

    Update MRapp.py

--- MRapp.py
+++ MRapp.py
@@ -1,95 +1,216 @@
-import streamlit as st
 import pandas as pd
+import numpy as np
+import streamlit as st
+import pydeck as pdk
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import euclidean_distances
+from scipy.spatial.distance import euclidean
 
-@st.cache_data
-def load_data():
-    df = pd.read_csv("clean_apartments.csv")
-    return df
-
-df = load_data()
-
-feature_cols = ['bedrooms', 'bathrooms', 'square_feet', 'pets?','latitude','longitude']
-
-#kmeans
-@st.cache_data
-def cluster_data(df, n_clusters=5):
-    X = df[feature_cols]
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-    
-    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
-    clusters = kmeans.fit_predict(X_scaled)
-    df['cluster'] = clusters
-    return df, kmeans, scaler
-
-#linreg
-@st.cache_data
-def train_regression_model(cluster_df):
-    cluster_df = cluster_df.dropna(subset=['price'])
-    
-    X = cluster_df[feature_cols]
-    y = cluster_df['price']
-    
-    model = LinearRegression()
-    model.fit(X, y)
-    return model
+# Import Cleaned Data
+apartments = pd.read_csv('clean_apartments.csv')
+apartments['cityname'] = apartments["cityname"].astype(str)
 
 
+# Page Title
 st.title('Rent Comparison Tool')
 st.markdown("Hayden Williford, Matthew Rostar, Jack Callahan, Lillian Bowling")
 
 st.header("Your Apartment Details:")
+
+# Create Fake Columns to get city name
+city_names = apartments['cityname']
+
+# Create Global Variables
+user_cluster = 0
+
+
+col12, col22, col32 = st.columns(3)
+with col12:
+    # Get User City Name
+    user_city = st.text_input("City Name?")
+
+    if user_city:
+        if user_city in city_names.values:
+            st.write(f"City Selected: {user_city}")
+        else:
+            st.error(f"{user_city} is not an approved city")
+            st.error("Please try another city")
+
+
 col1, col2, col3 = st.columns(3)
-
 with col1:
-    user_rent = st.text_input("Square FT")
-    if user_rent.isdigit():
-        user_rent = int(user_rent)
-    else:
-        user_rent = None
+    # User Rent Text Box
+    user_rent = st.text_input("Rent Amount?")
 
-    user_pets = st.selectbox("Pets Allowed?", ("No", "Yes"), index=None)
+    # Display the Rent value and restrict text to numbers
+    if user_rent:
+        if user_rent.isdigit():
+            number = int(user_rent)
+            st.write(f"Rent Amount: ${number}")
+        else:
+            st.error("Please enter a valid number")
+
+    # User Square Footage of Apartment Text Box
+    user_sqft = st.text_input("Square Footage?")
+
+    # Display the Square Footage Value and Restrict it to numbers
+    if user_sqft:
+        if user_sqft.isdigit():
+            number = int(user_sqft)
+            st.write(f"Square Footage: {user_sqft} ft²")
+            user_sqft = float(user_sqft)
+        else:
+            st.error("Please enter a valid whole number") 
+
 
 with col2:
+    # User Bedrooms Text Box
     user_bedrooms = st.text_input("Number of Bedrooms?")
-    if user_bedrooms.isdigit():
-        user_bedrooms = int(user_bedrooms)
-    else:
-        user_bedrooms = None
+
+    # Display the Bedrooms value and restrict text to numbers
+    if user_bedrooms:
+        try:
+            value = float(user_bedrooms)
+            if 0 <= value <= 4:
+                st.write(f"Number of Bedrooms: {value}")
+                user_bedrooms = float(user_bedrooms)
+            else:
+                st.error("Number must be between 0 and 4")
+        except ValueError:
+            st.error("Please enter a valid number.")
+
+    #Pets Allowed Drop Down
+    user_pets = st.selectbox("Pets Allowed?", 
+                             ("No","Yes"), 
+                             index = None)
+    if user_pets != None:
+        st.write(f"Pets Allowed: {user_pets}")
+
 
 with col3:
+    # User Bathrooms Text Box
     user_bathrooms = st.text_input("Number of Bathrooms?")
-    try:
-        user_bathrooms = float(user_bathrooms)
-    except ValueError:
-        user_bathrooms = None
 
-#what happens when user clicks on the predict rent button
-if st.button("Predict Rent"):
+    # Display the Number of Bathrooms value and restrict values
+    if user_bathrooms :
+        try:
+            value = float(user_bathrooms)
+            if 0 <= value <= 4 and (value * 2).is_integer():
+                st.write(f"Number of Bathrooms: {value}")
+                user_bathrooms = float(user_bathrooms)
+            else:
+                st.error("Number must be between 0 and 10, in 0.5 steps.")
+        except ValueError:
+            st.error("Please enter a valid number.")
+        
+# Feature Names of Training Data
+features = ['title', 'cityname', 'price', 'bedrooms', 'bathrooms', 'square_feet', 'pets?','longitude', 'latitude']
 
-    df_filtered = df[df['cityname'] == "Charlotte"]  # filter by user-selected city (add input for city)
-    
-    user_data = {
-        'bedrooms': user_bedrooms,
-        'bathrooms': user_bathrooms,
-        'square_feet': user_rent,  #not sure what rent amount is supposed to mean, but used square ft
-        'pets?': 1 if user_pets == "Yes" else 0,
-        'latitude':35.2016, #took random coordinates from charlotte apartment for placeholder
-        'longitude':-80.8124 
-    }
-    
-    user_df = pd.DataFrame([user_data])
-    
-    df_filtered = pd.concat([df_filtered, user_df], ignore_index=True)
-    df_filtered, kmeans, scaler = cluster_data(df_filtered)
-    
-    user_cluster = kmeans.predict(scaler.transform(user_df[feature_cols]))
-    similar_apartments = df_filtered[df_filtered['cluster'] == user_cluster[0]]
-    
-    model = train_regression_model(similar_apartments)
-    predicted_rent = model.predict(user_df[feature_cols])
-    
-    st.write(f"Predicted Rent for your apartment: ${predicted_rent[0]:,.2f}") 
+# Create User Dataframe
+user = pd.DataFrame({
+    "title": ['Users Apartment'],
+    "cityname": [user_city],
+    "price": [user_rent],
+    "bedrooms": [user_bedrooms],
+    "bathrooms": [user_bathrooms],
+    "square_feet": [user_sqft],
+    "pets?": [user_pets],
+    "longitude": [None],
+    "latitude": [None]
+})
+
+
+# Correct User_Pets from Yes and No to 1 and 0
+user['pets?'] = user['pets?'].replace({'Yes': 1, 'No': 0})
+
+
+# Make Training Dataset
+filtered_apartments = apartments[features]
+train_df = pd.concat([user, filtered_apartments]).reset_index(drop=True)
+
+# USER INDEX IS 0 (FIRST ROW IN TRAIN DF)
+record_index = 0
+
+# Create Dataframe for Map
+map_df = train_df[train_df['cityname'] == user_city].reset_index(drop=True)
+map_df = map_df.drop(columns = ['cityname', 'price', 'bedrooms', 'bathrooms', 'square_feet', 'pets?'], axis=1)
+# st.write("Dataset of The Map Data")
+# st.write(map_df)
+
+
+# Filter train_df to only the city the user entered
+train_df = train_df[train_df['cityname'] == user_city].reset_index(drop=True)
+train_df = train_df.drop(columns = ['cityname', 'price','longitude','latitude'], axis=1)
+
+# Display Dataset Before Kmeans
+# st.write('Dataset of User Entered Data and Apartment Dataset')
+# st.write(train_df.head())
+
+kmeans_features = ['bedrooms', 'bathrooms', 'square_feet', 'pets?']
+
+
+# Create Function to do Kmeans on Filtered dataframe
+def run_kmeans(df, columns, n_clusters = 5):
+
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(df[columns])
+
+    kmeans = KMeans(n_clusters= n_clusters, random_state = 0)
+    df['cluster'] = kmeans.fit_predict(X_scaled)
+
+    # Get User Cluster
+    user_cluster = df.iloc[0, 4]
+    user_point = df.loc[0, columns].values
+
+    # Filter Dataframe to only those in the users cluster
+    user_cluster_df = df[df['cluster'] == user_cluster].copy()
+
+    # Calculate Distance from user record
+    user_cluster_df['distance_from_user'] = user_cluster_df[columns].apply(lambda row: euclidean(row.values, user_point), axis=1)
+
+    return user_cluster_df
+
+
+
+# Create a Button to do K Means Clustering
+if st.button("Find Similar Apartments"):
+    kmeans_results = run_kmeans(train_df, columns= kmeans_features, n_clusters= 5)
+    st.write('Dataframe after Clustering')
+    #st.write(kmeans_results)
+
+    st.header("Top 5 Recommendations")
+    #st.write(kmeans_results.sort_values(by='distance_from_user').head(5))
+
+    top5 = kmeans_results.sort_values(by='distance_from_user').head(5)
+    st.write(top5)
+
+    top5_index = top5.index.tolist()
+    top5_with_coords = map_df.iloc[top5_index]
+    top5_with_coords = top5_with_coords.dropna(subset=['latitude', 'longitude'])
+
+
+    st.header("📍 Map of Top 5 Similar Apartments")
+
+    top5_with_coords = top5_with_coords.dropna(subset=['latitude', 'longitude'])
+
+    layer = pdk.Layer(
+        "ScatterplotLayer",
+        data=top5_with_coords,
+        get_position='[longitude, latitude]',
+        get_radius=100,
+        get_fill_color='[200, 30, 0, 160]',
+        pickable=True,
+    )
+
+    tooltip = {"html": "<b>{title}</b>", "style": {"color": "white"}}
+
+    view_state = pdk.ViewState(
+        latitude=top5_with_coords['latitude'].mean(),
+        longitude=top5_with_coords['longitude'].mean(),
+        zoom=11,
+        pitch=0
+    )
+
+    st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip=tooltip))
